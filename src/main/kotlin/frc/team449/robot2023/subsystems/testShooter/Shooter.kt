3c7daa0399b550b4d85d8fccfe73dccdfeecdbd1
Rakshay N <rakshay2108@gmail.com>
2023-10-14 16:51:57 -0400

    changed back to spark max pid (code for rafi)

--- src/main/kotlin/frc/team449/robot2023/subsystems/testShooter/Shooter.kt
+++ src/main/kotlin/frc/team449/robot2023/subsystems/testShooter/Shooter.kt
@@ -1,62 +1,49 @@
 package frc.team449.robot2023.subsystems.testShooter
 
+import com.revrobotics.CANSparkMax
+import com.revrobotics.CANSparkMaxLowLevel
 import edu.wpi.first.math.controller.PIDController
 import edu.wpi.first.math.controller.SimpleMotorFeedforward
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior.kCancelSelf
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.team449.system.encoder.NEOEncoder
+import frc.team449.system.motor.EncoderType
+import frc.team449.system.motor.SparkUtil
+import frc.team449.system.motor.WrappedSparkMax
 import frc.team449.system.motor.createSparkMax
 import io.github.oblarg.oblog.Loggable
 
 class Shooter(
   shooterId: Int,
-  pidConstants: Triple<Double, Double, Double>,
-  ffConstants: Triple<Double, Double, Double>,
-  private val goal: Double
 ): SubsystemBase(), Loggable {
 
-  private val motor = createSparkMax(
-    "shooter",
-    shooterId,
-    NEOEncoder.creator(1.0, 1.0)
-  )
+  private val shooterMotor = CANSparkMax(shooterId, CANSparkMaxLowLevel.MotorType.kBrushless)
+  private val shooterEncoder = shooterMotor.encoder
 
-  private val controller = PIDController(
-    pidConstants.first,
-    pidConstants.second,
-    pidConstants.third
+  init {
+    SparkUtil.applySparkSettings(
+      shooterMotor,
+      encoder = shooterEncoder,
+      gearing = 1.0,
+      unitPerRotation = 1.0,
     )
 
-  private val ff = SimpleMotorFeedforward(
-    ffConstants.first,
-    ffConstants.second,
-    ffConstants.third
-  )
+    SparkUtil.setPID(shooterMotor, Triple(6e-5, 0.0, 0.0))
+  }
 
   fun runShoot(): Command {
-    return this.runOnce {
-      motor.setVoltage(
-        controller.calculate(motor.velocity, goal) +
-          ff.calculate(goal)
-      )
-    }.repeatedly()
-      .withInterruptBehavior(kCancelSelf)
+    return this.runOnce { shooterMotor.pidController.setReference(5.0, CANSparkMax.ControlType.kVelocity)}
   }
 
   fun stopShoot(): Command {
-    return this.runOnce {
-      motor.setVoltage(0.0)
-    }
+    return this.runOnce { shooterMotor.stopMotor() }
   }
 
   companion object {
     fun createShooter(): Shooter {
       return Shooter(
         ShooterConstants.SHOOTER_ID,
-        ShooterConstants.kPID,
-        ShooterConstants.kFF,
-        ShooterConstants.GOAL
       )
     }
   }
